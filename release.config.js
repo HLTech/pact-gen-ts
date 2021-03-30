1b380e1734b8651a957a350d8b6e788c09bbec0e
Fiszcz <fiszczu@gmail.com>
2021-03-30 07:42:40 +0200

    ci: release configuration

---
+++ release.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/npm',
+        '@semantic-release/github',
+    ],
+    branches: [
+        'main',
+    ],
+};
