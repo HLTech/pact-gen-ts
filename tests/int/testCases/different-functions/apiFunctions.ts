bc36c289b0a43e4e2a6456820275212e9fa8da44
Fiszcz <fiszczu@gmail.com>
2021-04-29 09:10:05 +0200

    fix: analyze arrow variable functions

---
+++ tests/int/testCases/different-functions/apiFunctions.ts
@@ -0,0 +1,75 @@
+/**
+ * @pact
+ * @pact-method GET
+ * @pact-path /api/standard
+ */
+export function standardFunction() {
+    return 'string';
+}
+
+/**
+ * @pact
+ * @pact-method GET
+ * @pact-path /api/arrow
+ */
+export const arrowFunction = () => {
+    return 'string';
+};
+
+export const apiObject = {
+    /**
+     * @pact
+     * @pact-method GET
+     * @pact-path /api/object/property/function
+     */
+    objectPropertyFunction() {
+        return 'string';
+    },
+
+    /**
+     * @pact
+     * @pact-method GET
+     * @pact-path /api/object/property/arrow
+     */
+    objectPropertyArrowFunction: () => {
+        return 'string';
+    },
+
+    /**
+     * @pact
+     * @pact-method GET
+     * @pact-path /api/object/property/assigment-function
+     */
+    objectPropertyAssigmentFunction: function () {
+        return 'string';
+    },
+};
+
+export class ApiClass {
+    /**
+     * @pact
+     * @pact-method GET
+     * @pact-path /api/class/method
+     */
+    classMethodFunction() {
+        return 'string';
+    }
+
+    /**
+     * @pact
+     * @pact-method GET
+     * @pact-path /api/class/property/arrow
+     */
+    classPropertyArrowFunction = () => {
+        return 'string';
+    };
+
+    /**
+     * @pact
+     * @pact-method GET
+     * @pact-path /api/class/property/assigment-function
+     */
+    classPropertyAssigmentFunction = function () {
+        return 'string';
+    };
+}
